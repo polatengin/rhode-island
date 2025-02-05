10e065543cbfba269e5354a231f8e227af0e6432
polatengin <enpolat@microsoft.com>
2025-02-05 05:44:51 +0000

    protecting api calls with auth library

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,33 +1,20 @@
+import { auth } from "@/auth"
 import { users } from '@/schemas/drizzle';
 import { getRequestContext } from '@cloudflare/next-on-pages'
 import { drizzle } from 'drizzle-orm/d1';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
 
 export const runtime = 'edge'
 
-async function authenticate(req: Request) {
-  const session = await getServerSession(authOptions);
-
-  return !!session;
-}
-
-export async function GET(req: Request) {
-  if (!await authenticate(req)) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+export const GET = auth(async function GET(req: Request) {
   const { env } = getRequestContext();
   const db = drizzle(env.db);
 
   const userList = await db.select().from(users).all();
 
   return new Response(JSON.stringify(userList));
-}
+});
 
-export async function POST(req: Request) {
-  if (!await authenticate(req)) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+export const POST = auth(async function POST(req: Request) {
   const { env } = getRequestContext();
   const db = drizzle(env.db);
 
@@ -41,4 +28,4 @@ export async function POST(req: Request) {
   }).execute();
 
   return new Response(JSON.stringify({ id }), { status: 201 });
-}
+});
