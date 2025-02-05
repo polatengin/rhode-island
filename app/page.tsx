26229dc5612667aebac9c0de4f68b7f1f1d347d6
polatengin <enpolat@microsoft.com>
2025-02-05 04:10:11 +0000

    updating landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,63 @@
-import Image from "next/image";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { drizzle } from "drizzle-orm/d1";
+import { users } from "@/schemas/drizzle";
+import { useState } from "react";
 
-export default function Home() {
+export const runtime = "edge";
+
+export default async function Home() {
+
+  const { env } = getRequestContext();
+  const db = drizzle(env.db);
+
+  const [userList, setUserList] = useState<{
+    id: number;
+    organization_id: number;
+    name: string;
+    created_at: Date | null;
+    updated_at: Date | null;
+}[]>([]);
+
+  const getUserList = async () => {
+    const x = await db.select().from(users).all();
+    setUserList(x);
+  };
+
+  const saveUser = async () => {
+    await db.insert(users).values({
+      organization_id: 1,
+      name: `John Doe ${Math.floor(Math.random() * 1000)}`,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }).execute();
+    getUserList();
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
-            </code>
-            .
+            </code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <button onClick={saveUser}>Save</button>
+          <button onClick={getUserList}>Get</button>
         </ol>
+
+        <div>
+          <h1>Users</h1>
+          <ul>
+            {userList.map((user) => (
+              <li key={user.id}>
+                {user.name}
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -30,13 +66,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             Deploy now
           </a>
           <a
@@ -56,13 +85,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
           Learn
         </a>
         <a
@@ -71,13 +93,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
           Examples
         </a>
         <a
@@ -86,13 +101,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
           Go to nextjs.org →
         </a>
       </footer>
